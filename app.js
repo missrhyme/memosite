b00d2843f242b82ee15522e6708139a852ba8c08
彭博 <bopeng_h212@anjuke.com>
2016-10-08 18:15:02 +0800

    update static

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 require('./db/database');
 
+var path = require('path');
 var express = require('express');
 var swig = require('swig');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 var systemRoute = require('./app/routes/system');
 var initDatabase = require('./db/database');
@@ -16,6 +18,11 @@ app.set('view engine', 'html');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/css', express.static('./app/css'));
+app.use('/js', express.static('./app/js'));
+app.use('/lib', express.static('./app/lib'));
 
 app.use('/', systemRoute);
 
